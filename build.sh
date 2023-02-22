1094aed0aa02b98da27ab64230d90f4cef04af67
Yaroslav <garasym@gmail.com>
2023-02-23 01:55:14 +0200

    add jenkins-plugin-manager-2.12.11

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#git clone https://github.com/softservedata/devopsjen.git
+
+cd devopsjen
+java -jar jenkins-plugin-manager-2.12.11.jar --war jenkins.war --plugin-file plugins.txt --plugin-download-directory .jenkins/plugins
+
+docker build -t alpinejenkins .
+
+#docker run --rm --detach --name jenkins_server -p 8080:8080 alpinejenkins
