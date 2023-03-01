c70be7cce025d4e4b6128ee1dc91eb33f59a6b1f
Yaroslav <garasym@gmail.com>
2023-03-02 01:51:28 +0200

    add action

--- build.sh
+++ build.sh
@@ -3,8 +3,7 @@
 #git clone https://github.com/softservedata/devopsjen.git
 
 cd devopsjen
-java -jar jenkins-plugin-manager-2.12.11.jar --war jenkins.war --plugin-file plugins.txt --plugin-download-directory .jenkins/plugins
-
+java -jar jenkins-plugin-manager-2.12.11.jar --war jenkins.war --plugin-file plugins.txt --plugin-download-directory data/.jenkins/plugins
 docker build -t alpinejenkins .
 
 #docker run --rm --detach --name jenkins_server -p 8080:8080 alpinejenkins
